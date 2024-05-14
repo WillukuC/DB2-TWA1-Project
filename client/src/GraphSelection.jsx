41181fe267de6f675061bda74b2ca6ba8065dcf1
Willuku <le1952006@crc-lennox.qc.ca>
2024-05-13 20:35:42 -0400

    GraphSelection.jsx view 1

--- client/src/GraphSelection.jsx
+++ client/src/GraphSelection.jsx
@@ -1,9 +1,42 @@
+import { useState } from 'react'
 import React from 'react'
 
 function GraphSelection() {
-  return (
-    <div>GraphSelection</div>
-  )
+
+    const [graphType, setGraphType] = useState("");
+
+    return (
+        <div>
+            <a href="/" className='btn bg-light text-dark btn-outline-primary border-3 mt-4 position-absolute top-0 start-0 ms-4'>Back</a>
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-3">
+                        <div className="card btn border-3" onClick={() => setGraphType("line")} >
+                            <img src="/chart-line.png" className='mx-auto' alt="Line Chart" style={{ width: '50px' }} />
+                            <p>Line Chart</p>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="card btn border-3" onClick={() => setGraphType("bar")} >
+                            <img src="/chart-bar.png" className='mx-auto' alt="Bar Chart" style={{ width: '50px' }} />
+                            <p>Bar Chart</p>
+                        </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="card btn border-3" onClick={() => setGraphType("pie")} >
+                            <img src="/chart-pie.png" className='mx-auto' alt="Pie Chart" style={{ width: '50px' }} />
+                            <p>Pie Chart</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-3 row">
+                    <div className="col text-center">
+                        <div className="btn bg-light text-dark btn-outline-primary border-3" onClick={() => console.log(graphType)}>Select Graph Type</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default GraphSelection
