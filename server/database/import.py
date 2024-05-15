9f7fa02e671dde369aa99c749c00c9336b3367d2
GonzaloBarrera <113914262+GonzaloBarrera@users.noreply.github.com>
2024-05-15 17:34:16 -0400

    import.py mofification

--- server/database/import.py
+++ server/database/import.py
@@ -5,16 +5,25 @@ import sys
 arguments = sys.argv
 
 def import_data_csv(owid, country):
-    country = pd.read_csv('countries.csv')
-    owid = pd.read_csv('owid-energy-data_A_S.csv')
-    filtered = owid[owid['country'].isin(country['country'])]
+    country_data = pd.read_csv(country)
+    owid_data = pd.read_csv(owid)
+    filtered = owid_data[owid_data['country'].isin(country_data['country'])]
 
     client = py.MongoClient('mongodb+srv://gonzaloba2002:Mt6o7Cd2SzSTM3Mw@cluster0.wwmzyc9.mongodb.net/')
     db = client['energy_consumption']
+
     collection = db['country_energy']
 
-    data = filtered.to_dict('records')
+    existing_countries = collection.distinct("country")
+    
+    new_data = filtered[~filtered['country'].isin(existing_countries)]
 
-    collection.insert_many(data)
+    if not new_data.empty:
+        new_data_records = new_data.to_dict('records')
+        collection.insert_many(new_data_records)
+        print("New data added successfully.")
+    else:
+        data = filtered.to_dict('records')
+        collection.insert_many(data)
 
 import_data_csv(arguments[1], arguments[2])
